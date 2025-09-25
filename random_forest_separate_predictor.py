36d11d7b39a278ac4f8e31df111de96db3cd03bd
BezLiz <lizabezzubova@icloud.com>
2025-09-25 20:08:32 +0300

    Доработала RF часть в main

--- random_forest_separate_predictor.py
+++ random_forest_separate_predictor.py
@@ -2,7 +2,7 @@ import pandas as pd
 import numpy as np
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.model_selection import train_test_split
-from sklearn.metrics import mean_squared_error, r2_score
+from sklearn.metrics import mean_squared_error, r2_score, mean_absolute_error
 
 # NB: Для реального submission, конечно, не тянет, но это эксперимент
 # Задним числом выясняется, что даже авторы контеста используют её для бейзлайна
@@ -76,8 +76,9 @@ class RandomForestPredictor:
 
             mse = mean_squared_error(y_test, y_pred)
             r2 = r2_score(y_test, y_pred)
+            mae = mean_absolute_error(y_test, y_pred)
 
-            results[target] = {"MSE": mse, "R2": r2, "n_samples": len(df_target)}
+            results[target] = {"MSE": mse, "MAE": mae, "R2": r2, "n_samples": len(df_target)}
 
         return results
 
